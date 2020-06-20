87ebd3a55f9e19724f3f9046fa831a4275e363ad
Kamil Jurowicz <kamil.jurowicz@rawlplug.com>
2020-06-20 12:46:09 +0200

    add responsive headline

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,13 +1,14 @@
 <script>
     import Vue from 'vue';
-    import { headlineMain, headline1, mobileHeadline1 } from "../src/entry";
+    import { headlineMain, headline1, mobileHeadline1, responsiveHeadline } from "../src/entry";
 
     export default Vue.extend({
         name: 'ServeDev',
         components: {
             headlineMain,
             headline1,
-            mobileHeadline1
+            mobileHeadline1,
+            responsiveHeadline
         }
     });
 </script>
@@ -18,6 +19,6 @@
         <headline-main forced-down="true">This is headline main with forced size down.</headline-main>
         <headline-1>I'm headline 1 for landing pages</headline-1>
         <mobile-headline-1>I'm mobile version of headline1</mobile-headline-1>
-      <responsive-headline-1>Responsive headline 1</responsive-headline-1>
+        <responsive-headline>My name is responsive headline and I change size if viewport is under 576px</responsive-headline>
     </div>
 </template>
